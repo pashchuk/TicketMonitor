dbe241174ea715ca2a362de65e3238ddd176453b
Eduard <mamaxropela@gmail.com>
2014-11-24 20:23:37 +0200

    add station class

--- UzAPI/UzAPI.cs
+++ UzAPI/UzAPI.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace UzAPI
 {
-	public class UzAPI
+	public class MainAPI
 	{
+		private List<string> _fromPoint;
+		private List<string> _toPoint;
+		
+	}
+	[JsonObject]
+	public class Station
+	{
+		[JsonProperty(PropertyName = "station_id")]
+		public int StationID { get; private set; }
+		[JsonProperty(PropertyName = "title")]
+		public string Name { get; private set; }
 	}
 }
